fb3b8ab5b9a61c23c90af394c1105efe1bee10ec
franky-collab <waruif94@gmail.com>
2025-02-03 13:41:08 +0300

    new commit

---
+++ Variable.py
@@ -0,0 +1,16 @@
+#Variable is a container used to store a value
+
+number=10
+firstname="Michael"
+lastname="Wekesa"
+a=10
+b=15
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+language="Python"
+print(language,"is a web programming language")
+print(a+b)
+#The sum of 10 and 15 is 25
+print("The sum of 10 and 15 is 25")
---
+++ introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display a value
+
+print("Michael")
+print("eMobilis")
+print(39)
+print(100*2)
