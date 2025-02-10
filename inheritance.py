23409ef8549f5b4e7c5c6395331f6a7c4629759c
franky-collab <waruif94@gmail.com>
2025-02-10 13:31:32 +0300

    fourth commit

---
+++ inheritance.py
@@ -0,0 +1,20 @@
+#Parent/Super/Base class
+class Animal:
+    def speak(self):
+        print("Animal is speaking")
+
+#Child/Sub/Derived class
+class Dog(Animal):
+    def bark(self):
+        print("Dog is barking")
+
+class Cat(Dog):
+    def meow(self):
+        print("Cat is meowing")
+
+
+a=Animal()
+d=Dog()
+d.speak()
+c=Cat()
+c.speak()
